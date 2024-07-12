14a25c2ef01a9479cb069c09140e9b1a8ab3d0ef
Daniel Picon <75026318+LastDaniels@users.noreply.github.com>
2024-07-12 01:45:24 -0500

    Delete java directory

--- java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cecy.proyecto1grupo8 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens proyecto1.grupo13 to javafx.fxml;
-    exports proyecto1.grupo13;
-}
